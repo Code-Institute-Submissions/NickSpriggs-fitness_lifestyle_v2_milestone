5cb7b1f9bf26adbe5fef64fecbecb55c18e5f871
NickSpriggs <ntspriggs@me.com>
2022-01-18 02:47:33 +0000

    Added notification css, shopping bag preview, and additional messages

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -13,7 +13,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a specified product to the users shopping bag """
 
-    product = Product.objects.get(pk = item_id)
+    product = get_object_or_404(Product, pk=item_id)
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
@@ -27,14 +27,17 @@ def add_to_bag(request, item_id):
 
 def remove_from_bag(request, item_id):
     """ remove a specified product to the users shopping bag """
+    product = get_object_or_404(Product, pk=item_id)
 
     try:
         bag = request.session.get('bag', {})
         
         bag.pop(item_id)
+        messages.success(request, f'Removed: {product.name}')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error when attempting to remove: {product.name}')
         return HttpResponse(status=500)
