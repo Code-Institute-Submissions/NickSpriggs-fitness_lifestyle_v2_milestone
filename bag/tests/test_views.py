0cefeeff67230782a938ed1c4f13cfcd458dd1e4
NickSpriggs <ntspriggs@me.com>
2022-02-22 17:34:52 +0000

    Created automated testing files for bag, checkout, home, products, and profiles

---
+++ bag/tests/test_views.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+
+class TestBagViews(TestCase):
+    ''' Test Bag Views '''
+
+    def test_view_bag_view(self):
+        '''Testing the view_bag view'''
+
+        response = self.client.get('/bag/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'bag/bag.html')
