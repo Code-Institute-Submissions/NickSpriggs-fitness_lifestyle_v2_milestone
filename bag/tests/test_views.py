d7adebb6481ce46487d5e3250ac5ec2029e728cb
NickSpriggs <ntspriggs@me.com>
2022-02-22 18:12:14 +0000

    Completed home and bag tests

--- bag/tests/test_views.py
+++ bag/tests/test_views.py
@@ -4,9 +4,9 @@ from django.test import TestCase
 class TestBagViews(TestCase):
     ''' Test Bag Views '''
 
-    def test_view_bag_view(self):
+    def test_view_bag(self):
         '''Testing the view_bag view'''
 
-        response = self.client.get('/bag/')
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'bag/bag.html')
